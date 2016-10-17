deb40e470f02198576046cc93a87ed0f5a9019d9
KavithaVishwanathan <kavivisu1612@gmail.com>
2016-10-17 16:00:35 -0400

    Added final document

--- feedbackAvg.py
+++ feedbackAvg.py
@@ -3,17 +3,17 @@ def calculateFeedbackAvg (int feedbackId):
 	int avg = 0
 	for id in sID :
 	 avg = avg + calculateSurveyAvg(id)     #calculate the average for each survey
-
+	 
 	return avg/len(sid)
 
 def getSurveyIds(int id):
-   int[] ids = executeCommand("SELECT s.survey_id FROM feedback as f INNER JOIN survey  as s on f.feedback_id = s.feedback_id WHERE f.feedback_id = %d", id)
+   int[] ids = executeCommand("SELECT s.survey_id FROM feedback AS f INNER JOIN survey  AS s ON f.feedback_id = s.feedback_id WHERE f.feedback_id = %d", id)
    return ids
 
 def calculateSurveyAvg(int sId):
-    int[] answers =  executeCommand("SELECT s.answer FROM survey as s, survey_answers as sa WHERE s.survey_id = %d", sId)
+    int[] answers =  executeCommand("SELECT s.answer FROM survey AS s, survey_answers AS sa WHERE s.survey_id = %d", sId)
 	int sum = 0
 	for ans in answers:
-	     sum = sum + (ans * 2) 
+	     sum = sum + ans 
 	
 	return sum/len(answers)
